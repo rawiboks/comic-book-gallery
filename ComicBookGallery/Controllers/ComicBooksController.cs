2d4d7771a490d02a2985b9da265a7c6bf9502718
rawiboks <piotr91@hotmail.co.uk>
2019-02-05 22:57:53 +0000

    updated comic book detail view layout

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -24,7 +24,7 @@ namespace ComicBookGallery.Controllers
                     new Artist(){Name="Edgar Delgado", Role="Colors"},
                     new Artist(){Name="Chris Eliopoulos", Role="Letters"},
                 }
-        };
+            };
 
             return View(comicBook);
         }
